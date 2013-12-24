e672db3e5ba8e89b4746dfd9b2fea37c91038242
Shahar Talmi <shahart@wix.com>
2013-12-24 20:53:19 +0200

    removed retries

--- util/bower.js
+++ util/bower.js
@@ -32,28 +32,9 @@ function findJSMainFile(component, main) {
   return component.replace(/js$/i, '') + '.js';
 }
 
-var queue = [];
-var pending = 0;
-
 bowerUtil.resolveMainPath = function resolveMain(component, version, callback) {
-  if (pending > 4) {
-    queue.push(resolveMain.bind(this, component, version, callback));
-    return false;
-  }
-  pending++;
-
   debug('resolving main property for component %s#%s', component, version);
   bower.commands.info(component + '#' + version, 'main').on('end', function (main) {
-    pending--;
-    if (queue.length > 0) {
-      (queue.shift())();
-    }
     callback(null, bowerUtil.joinComponent(component, findJSMainFile(component, main)));
-  }).on('error', function (e) {
-    pending--;
-    if (queue.length > 0) {
-      (queue.shift())();
-    }
-    callback(e);
-  });
+  }).on('error', callback);
 };
