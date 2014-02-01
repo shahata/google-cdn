9cb9903f200a19194eab0183a077ac54cb161ebc
Shahar Talmi <shahart@wix.com>
2014-02-01 22:21:21 +0200

    try to handle dup requests without asking bower

--- util/bower.js
+++ util/bower.js
@@ -4,6 +4,7 @@ var bower = require('bower');
 var path = require('path');
 var debug = require('debug')('google-cdn');
 var bowerUtil = module.exports;
+var cache = {};
 
 
 bowerUtil.joinComponent = function joinComponent(directory, component) {
@@ -34,8 +35,23 @@ function findJSMainFile(component, main) {
 
 bowerUtil.resolveMainPath = function resolveMain(component, version, callback) {
   debug('resolving main property for component %s#%s', component, version);
-  //bower.reset();
-  bower.commands.info(component + '#' + version, 'main').on('end', function (main) {
+  var str = component + '#' + version;
+  var listener = function (main) {
     callback(null, bowerUtil.joinComponent(component, findJSMainFile(component, main)));
-  }).on('error', callback);
+  };
+  if (cache[str]) {
+    if (cache[str].result !== undefined) {
+      setTimeout(listener.bind(this, cache[str].result), 0);
+    } else {
+      cache[str].listeners.push(listener);
+    }
+  } else {
+    cache[str] = {listeners: [listener]};
+    bower.commands.info(str, 'main').on('end', function (main) {
+      cache[str].result = main;
+      cache[str].listeners.forEach(function (listener) {
+        listener(main);
+      });
+    }).on('error', callback);
+  }
 };
